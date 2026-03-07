71560758908e9ab1dac873622f3dde84fb6c5640
mastery0 <giulio.dibartolo23@gmail.com>
2026-03-07 16:36:21 +0100

    Update data.cs

--- Assets/Scripts/data.cs
+++ Assets/Scripts/data.cs
@@ -5,7 +5,10 @@ public class data : MonoBehaviour
 
     public static int money;
     public static bool baseUP;
+<<<<<<< Updated upstream
     public static int baseUPlvl;
+=======
+>>>>>>> Stashed changes
     public static int critUPlvl;
     public static int critValue;
 }

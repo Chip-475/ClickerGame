3f2d585fbc5a8f2b1fe75c16dbdab11cfc4f9d6a
mastery0 <giulio.dibartolo23@gmail.com>
2026-03-09 00:42:44 +0100

    screen safeArea,critperk

--- Assets/Scripts/data.cs
+++ Assets/Scripts/data.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class data : MonoBehaviour
 {
 
-    public static int money = 100000;
+    public static int money;
     public static int baseUPlvl;
-    public static int critUPlvl;
+    public static int critUPlvl=1;
+    public static int critValue=2;
 
 }

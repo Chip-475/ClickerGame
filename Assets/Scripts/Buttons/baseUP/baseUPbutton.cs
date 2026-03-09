6bd8d55119eb0c2274b7140b8bc25ea050c0fd52
vittori02-ui <vittorioprivitera02@gmail.com>
2026-03-09 01:51:12 +0100

    modifica3_vitto

--- Assets/Scripts/Buttons/baseUP/baseUPbutton.cs
+++ Assets/Scripts/Buttons/baseUP/baseUPbutton.cs
@@ -14,30 +14,35 @@ public class baseUPbutton : MonoBehaviour
                 data.baseUPlvl++;
                 manager.baseUPeffect++;
                 manager.baseUPcost = 100;
+                data.money -= 100;
                 return;
             case 1:
                 clicker.clickStr++;
                 data.baseUPlvl++;
                 manager.baseUPeffect++;
                 manager.baseUPcost = 250;
+                data.money -= 250;
                 return;
             case 2:
                 clicker.clickStr++;
                 data.baseUPlvl++;
                 manager.baseUPeffect++;
                 manager.baseUPcost = 1000;
+                data.money -= 1000;
                 return;
             case 3:
                 clicker.clickStr++;
                 data.baseUPlvl++;
                 manager.baseUPeffect++;
                 manager.baseUPcost = 5000;
+                data.money -= 5000;
                 return;
             case 4:
                 clicker.clickStr++;
                 data.baseUPlvl++;
                 manager.baseUPeffect++;
                 manager.baseUPcost = 10000;
+                data.money -= 10000;
                 return;
         }
     }
--- Assets/Scripts/clicker.cs
+++ Assets/Scripts/clicker.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
-
+using TMPro;
 public class clicker : MonoBehaviour
 {
 
     public static int clickStr = 1;
+    public GameObject testo;
+    public Transform bottone;
+
 
     public void click()
     {
@@ -11,10 +14,24 @@ public class clicker : MonoBehaviour
     }
     public void crit()
     {
-        int r = UnityEngine.Random.Range(0, 100);
+        int r = UnityEngine.Random.Range(0, 10);
         if (r <= data.critUPlvl)
         {
-            data.money += clickStr * 5;
+            data.money += clickStr * 5; 
+            testo.SetActive(true);
+            float spstX, spstY;
+            do
+            {
+                spstX = Random.Range(-100f, 100f);  
+                spstY = Random.Range(-100f, 100f);
+            } while (spstX > -30f && spstX < 30 && spstY > -30f && spstY < 30f);  //accusi non si mette sopra il bottone e ritenta
+            testo.transform.position = bottone.position + new Vector3(spstX, spstY, 0);
+            Invoke("disattiva",1f); //per aspettare
         }
+    }
+
+    public void disattiva()
+    {
+        testo.SetActive(false);
     }
 }

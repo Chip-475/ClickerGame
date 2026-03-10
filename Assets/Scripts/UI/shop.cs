758d06eaea0f67cde4bb324472ad954a84b47944
mastery0 <giulio.dibartolo23@gmail.com>
2026-03-11 00:44:17 +0100

    shopbg,autoclicker

--- Assets/Scripts/UI/shop.cs
+++ Assets/Scripts/UI/shop.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class shop : MonoBehaviour
 {
     public GameObject shopPanel;
+    public GameObject shopBG;
     public void OpenShop()
     {
         if(shopPanel.activeSelf)
         {
             shopPanel.SetActive(false);
+            shopBG.SetActive(false);
         }
         else
         {
             shopPanel.SetActive(true);
+            shopBG.SetActive(true);
         }
         
     }

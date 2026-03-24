2a07b2406cd40ba34ae89044906fb9b4cf2a50e2
vittori02-ui <vittorioprivitera02@gmail.com>
2026-03-24 18:32:45 +0100

    modifica9_vitto

--- Assets/Scripts/Pets/SuperPets.cs
+++ Assets/Scripts/Pets/SuperPets.cs
@@ -1,17 +1,23 @@
-using UnityEngine;
+using UnityEngine; 
+public enum rarita
+{
+    common,
+    rare,
+    leggend
+}
 public abstract class SuperPets : MonoBehaviour
 {
     [SerializeField] private GameObject buttonLevelUP;
     protected int level; // da 1 a 10
-    protected int rank;  // da 1-3
-    protected int rarity; // -1 common    0 rari   1 leggendari
+    protected int rank;  // da 1-3 
+    [SerializeField] protected rarita rar; // -1 common    0 rari   1 leggendari
     protected float stamina=100f; // da 0 a 100;
     protected float staminaRate=10f;
 
 
     public int getLevel() { return level; }
     public int getRank() { return rank; }
-    public int getRarity() { return rarity; }
+    //public rarita getRarity() { return rarity; }
     public float getStamina() { return stamina; }
 
     // abstract protected void livella(); //serve il button

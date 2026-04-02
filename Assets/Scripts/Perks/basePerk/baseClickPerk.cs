a1273ed0e38e4c7c3e6574bf0e1c6e7ec1a1d082
Chip-475 <gioelegarozzo2008@gmail.com>
2026-04-02 14:19:09 +0200

    optimization

--- Assets/Scripts/Perks/basePerk/baseClickPerk.cs
+++ Assets/Scripts/Perks/basePerk/baseClickPerk.cs
@@ -3,21 +3,18 @@ using System.Collections;
 public class baseClickPerk : MonoBehaviour
 {
     public float duration = 30f;
-    public bool isActive = false;
+    static public bool isActive = false;
     public IEnumerator ClickPerk()
     {
+        data.clickPerk--;
         isActive = true;
         clicker.clickStr *= 2;
         yield return new WaitForSeconds(duration);
-        isActive = false;
         clicker.clickStr /= 2;
+        isActive = false;
     }
     public void onClick()
     {
-        if (!isActive)
-        {
-            StartCoroutine(ClickPerk());
-            data.clickPerk--;
-        }
+        StartCoroutine(ClickPerk());
     }
 }

da3d95235bd1823b45bc953717c6d2164b4e57a5
mastery0 <giulio.dibartolo23@gmail.com>
2026-04-22 18:32:21 +0200

    dropdown and animations

--- Assets/Scripts/Managers/xpManager.cs
+++ Assets/Scripts/Managers/xpManager.cs
@@ -1,4 +1,4 @@
-using System.Data;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,47 +8,98 @@ public class xpManager : MonoBehaviour
     public Image xpBar;
     public TMP_Text lvl;
     public TMP_Text exp;
+    [Header("Level Up UI")]
+    public GameObject levelUpPanel;
+    public TMP_Text levelUpText;
+    public CanvasGroup levelUpCanvas;
     void Unlock()
     {
         switch (data.lvl)
         {
             case 5:
-                //petlimit=1
                 break;
             case 10:
-                //petlimit=2
                 break;
             case 20:
-                //petlimit=3
                 break;
-
         }
+
         data.money += data.lvl * 50;
+
         if (data.lvl % 5 == 0)
         {
             data.PerkLimit++;
         }
     }
+
     void Update()
     {
-        xpBar.fillAmount = (float)data.xp/ data.xpMax;
-        lvl.text = "livello:"+data.lvl;
-        exp.text = data.xp+"/"+data.xpMax;
+        xpBar.fillAmount = (float)data.xp / data.xpMax;
+        lvl.text = "livello:" + data.lvl;
+        exp.text = data.xp + "/" + data.xpMax;
+
         if (data.xp >= data.xpMax)
         {
             Unlock();
+
+            data.xp -= data.xpMax;
+            data.lvl += 1;
+
             if (data.lvl < 10)
-            {
-                data.xp -= data.xpMax;
-                data.lvl += 1;
                 data.xpMax += data.xpMax / 3;
-            }
             else
-            {
-                data.xp -= data.xpMax;
-                data.lvl += 1;
                 data.xpMax = (int)(100 * Mathf.Pow(data.lvl, 1.5f));
-            }
+            StartCoroutine(LevelUpAnimation());
         }
+    }
+
+    IEnumerator LevelUpAnimation()
+    {
+        levelUpPanel.SetActive(true);
+        RectTransform rect = levelUpPanel.GetComponent<RectTransform>();
+        
+        if (data.lvl % 5 == 0)
+        {
+            if(data.lvl!=5&&data.lvl!=10&&data.lvl!=20) levelUpText.text = "Level: " + data.lvl + "<br>Obtained:" + data.lvl * 50+"<br>New Perk Cap:"+data.PerkLimit;
+            switch (data.lvl)
+            {
+                case 5:
+                    levelUpText.text = "Level: " + data.lvl + "<br>Obtained:" + data.lvl * 50 + "<br>New Perk Cap:" + data.PerkLimit + "<br>Pet Unlocked";
+                    break;
+                case 10:
+                    levelUpText.text = "Level: " + data.lvl + "<br>Obtained:" + data.lvl * 50 + "<br>New Perk Cap:" + data.PerkLimit + "<br>2nd Pet Slot Unlocked";
+                    break;
+                case 20:
+                    levelUpText.text = "Level: " + data.lvl + "<br>Obtained:" + data.lvl * 50 + "<br>New Perk Cap:" + data.PerkLimit + "<br>3rd Pet Slot Unlocked";
+                    break;
+            }
+        }//text handling
+        else
+        {
+            levelUpText.text = "Level: " + data.lvl+"<br>Obtained:"+data.lvl*50;
+        }
+        float duration = 0.3f;
+        float t = 0;
+        rect.localScale = Vector3.zero;
+        levelUpCanvas.alpha = 0;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float progress = t / duration;
+            rect.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, progress);
+            levelUpCanvas.alpha = progress;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.8f);
+        t = 0;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float progress = t / duration;
+            levelUpCanvas.alpha = 1 - progress;
+            yield return null;
+        }
+
+        levelUpPanel.SetActive(false);
     }
 }
--- Assets/Scripts/meteor.cs
+++ Assets/Scripts/meteor.cs
@@ -1,4 +1,3 @@
-using System.Buffers.Text;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -9,46 +8,129 @@ public class meteor : MonoBehaviour
     public TMP_Text metHp;
     public static int hpMeteor;
     public static int hpMaxMeteor;
-    bool x = true;
-    private void meteorLvl()
+    bool isAlive = true;
+    public RectTransform rect;
+    Vector2 targetPos = Vector2.zero;
+    void Start()
     {
-        data.meteorCrushed++;
-        data.meteorlvl = (int)(15 * (1 + data.meteorCrushed/3 * 0.05f) * Mathf.Pow(1.05f, data.meteorCrushed/3));
+        ResetMeteor();
+        StartCoroutine(FallAndBounce());
     }
-    IEnumerator Meteor()
+
+    void Update()
     {
-        data.money += hpMaxMeteor; //* petManager.globalMoneyMod;
+        metHp.text = hpMeteor + "/" + hpMaxMeteor;
+
+        if (isAlive && hpMeteor <= 0)
+        {
+            isAlive = false;
+            StartCoroutine(DeathAnimation());
+        }
+    }
+
+    void ResetMeteor()
+    {
+        hpMaxMeteor = Random.Range(data.meteorlvl * 3, data.meteorlvl * 5);
+        hpMeteor = hpMaxMeteor;
+        rect.localScale = Vector3.one;
+        rect.anchoredPosition = new Vector2(Random.Range(-300f, 300f), 1000f);
+
+        isAlive = true;
+    }
+
+    IEnumerator FallAndBounce()
+    {
+        float speed = 2000f;
+        float bounceHeight = 150f;
+        while (rect.anchoredPosition.y > targetPos.y)
+        {
+            rect.anchoredPosition = Vector2.MoveTowards(
+                rect.anchoredPosition,
+                targetPos,
+                speed * Time.deltaTime
+            );
+            yield return null;
+        }
+        Vector2 bounceTarget = targetPos + Vector2.up * bounceHeight;
+        while (rect.anchoredPosition.y < bounceTarget.y)
+        {
+            rect.anchoredPosition = Vector2.MoveTowards(
+                rect.anchoredPosition,
+                bounceTarget,
+                speed * Time.deltaTime
+            );
+            yield return null;
+        }
+        while (rect.anchoredPosition.y > targetPos.y)
+        {
+            rect.anchoredPosition = Vector2.MoveTowards(
+                rect.anchoredPosition,
+                targetPos,
+                speed * Time.deltaTime
+            );
+            yield return null;
+        }
+
+        rect.anchoredPosition = targetPos;
+    }
+
+    IEnumerator DeathAnimation()
+    {
+        float popTime = 0.1f;
+        float t = 0;
+
+        Vector3 start = Vector3.one;
+        Vector3 pop = Vector3.one * 1.4f;
+
+        while (t < popTime)
+        {
+            t += Time.deltaTime;
+            rect.localScale = Vector3.Lerp(start, pop, t / popTime);
+            yield return null;
+        }
+        float shrinkTime = 0.25f;
+        t = 0;
+
+        while (t < shrinkTime)
+        {
+            t += Time.deltaTime;
+            rect.localScale = Vector3.Lerp(pop, Vector3.zero, t / shrinkTime);
+            yield return null;
+        }
+
+        yield return StartCoroutine(MeteorRespawn());
+    }
+
+    IEnumerator MeteorRespawn()
+    {
+        data.money += hpMaxMeteor;
+
         if (goldMeteorPerk.isActive)
         {
-            data.money += hpMaxMeteor; //*petManager.globalMoneyMod * 4;
-            goldMeteorPerk.isActive=false;
+            data.money += hpMaxMeteor;
+            goldMeteorPerk.isActive = false;
         }
+
         met.SetActive(false);
+
         meteorLvl();
-        hpMaxMeteor = Random.Range(data.meteorlvl * 3, data.meteorlvl * 5);
-        yield return new WaitForSeconds(2);
+
+        yield return new WaitForSeconds(1.5f);
+
+        ResetMeteor();
         met.SetActive(true);
-        hpMeteor = hpMaxMeteor;
-        x = true;
+
+        StartCoroutine(FallAndBounce());
     }
 
-    private void Start()
+    void meteorLvl()
     {
-        hpMaxMeteor = Random.Range(data.meteorlvl * 3, data.meteorlvl * 5);
-        hpMeteor = hpMaxMeteor;
-    }
+        data.meteorCrushed++;
 
-    public void Update()
-    {
-        metHp.text =hpMeteor +"/"+ hpMaxMeteor;
-        if(x && hpMeteor <= 0)
-        {
-            transform.localScale -= Vector3.one * 2f * Time.deltaTime;
-            if (transform.localScale.x < 0)
-            {
-                StartCoroutine(Meteor());
-                x = false;
-            }
-        }
+        data.meteorlvl = (int)(
+            15 *
+            (1 + data.meteorCrushed / 3f * 0.05f) *
+            Mathf.Pow(1.05f, data.meteorCrushed / 3f)
+        );
     }
 }

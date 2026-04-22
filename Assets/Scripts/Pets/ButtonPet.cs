20694365f7b0a39be22c1e1604dec4925de534ea
Chip-475 <gioelegarozzo2008@gmail.com>
2026-04-22 15:53:28 +0200

    Revert "modifica 15"
    This reverts commit 22f5847cea703c52833e1e523726eb72a5b313dd.

--- Assets/Scripts/Pets/ButtonPet.cs
+++ Assets/Scripts/Pets/ButtonPet.cs
@@ -3,20 +3,18 @@ using UnityEngine.UI;
 public class ButtonPet : MonoBehaviour
 {
     [SerializeField] private GameObject buttonLevelUp;
-    public SottoPet pet;
+    public SuperPets pet;
     void Update()
     {
-        if (buttonLevelUp==null) return;
-        if (pet==null) return;
-        buttonLevelUp.SetActive(true);
+        if (buttonLevelUp != null) return;
+        if (pet == null) return;
+
+        buttonLevelUp.SetActive(pet.PuoLevelUp());
     }
     public void LevelUp()
     {
-        Debug.Log("Sono qui");
-        if (pet==null) return;
+        if (pet == null) return;
         pet.ProvaLevelUp();
-        Debug.Log("Sono qui");
-        //Debug.Log("PuoUppare: "+pet.PuoLevelUp());
     }
 
 }

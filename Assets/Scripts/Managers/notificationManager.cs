eee170850857493aac41efc08a22ce1869c0c7cb
mastery0 <giulio.dibartolo23@gmail.com>
2026-05-16 12:04:27 +0200

    notif fix

--- Assets/Scripts/Managers/notificationManager.cs
+++ Assets/Scripts/Managers/notificationManager.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
-using Unity.Notifications.Android;
-using System.Data;
 using System;
 using System.Collections;
+using Unity.Notifications.Android;
 
 public class notificationManager : MonoBehaviour
 {
     public string ChannelID = "game_reminders";
+
+    private bool permissionRequestInProgress;
+
     private void Start()
     {
         var channel = new AndroidNotificationChannel
@@ -17,14 +19,20 @@ public class notificationManager : MonoBehaviour
             Description = "Reminders for keep playing our awesome game!"
         };
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        StartCoroutine(RequestPermission());
     }
 
     public void ScheduleNotif()
     {
+        if (AndroidNotificationCenter.UserPermissionToPost != PermissionStatus.Allowed)
+        {
+            return;
+        }
+
         var notification1 = new AndroidNotification
         {
-            Title = "Get rid of these meteors",
-            Text = "Come back and destroy some meteors!",
+            Title = "Offline bonus has been capped",
+            Text = "Come back and collect your money!",
             FireTime = DateTime.Now.AddHours(4),
         };
         var notification2 = new AndroidNotification
@@ -39,14 +47,16 @@ public class notificationManager : MonoBehaviour
             Text = "Haven't seen you in a while...",
             FireTime = DateTime.Now.AddDays(2),
         };
-        AndroidNotificationCenter.SendNotification(notification1,ChannelID);
+        AndroidNotificationCenter.SendNotification(notification1, ChannelID);
         AndroidNotificationCenter.SendNotification(notification2, ChannelID);
         AndroidNotificationCenter.SendNotification(notification3, ChannelID);
     }
+
     public void ClearNotification()
     {
         AndroidNotificationCenter.CancelAllNotifications();
     }
+
     public void OnApplicationPause(bool pause)
     {
         if (pause)
@@ -58,17 +68,27 @@ public class notificationManager : MonoBehaviour
             ClearNotification();
         }
     }
+
     public IEnumerator RequestPermission()
     {
-        if(AndroidNotificationCenter.UserPermissionToPost==PermissionStatus.Allowed)
+        if (permissionRequestInProgress)
         {
             yield break;
         }
+
+        if (AndroidNotificationCenter.UserPermissionToPost == PermissionStatus.Allowed)
+        {
+            yield break;
+        }
+
+        permissionRequestInProgress = true;
         var request = new PermissionRequest();
         while (request.Status == PermissionStatus.RequestPending)
         {
             yield return null;
         }
-        
+
+        permissionRequestInProgress = false;
+        yield break;
     }
 }
